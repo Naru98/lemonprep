2fc5fef9fa95c7ed2ffb3bdf75c24e0b79550cd1
Narayan Rahul <rawoolnar98@gmail.com>
2021-02-27 06:06:05 +0530

    fixed issue

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/gym';
+//$config['base_url'] = 'http://localhost/lemonprep';
+$config['base_url'] = 'https://lemonprep.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
