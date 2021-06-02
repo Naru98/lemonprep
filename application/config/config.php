d6996c46494656c0dca450745036448f35a2cc10
Narayan Rahul <43835215+Naru98@users.noreply.github.com>
2021-06-02 21:28:53 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/lemonprep';
-$config['base_url'] = 'https://lemonprep.herokuapp.com/';
+$config['base_url'] = 'https://lemonprep.lemonkiosk.com/';
 
 /*
 |--------------------------------------------------------------------------
